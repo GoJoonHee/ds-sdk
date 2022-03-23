c6e3fff2754edb947099f12ad1402fb234c125c4
dong.liping3 <Dlp.920707>
2022-03-23 16:03:28 +0800

    11

---
+++ src/main/java/com/bluecloud/vnet/sdk/java/dssdk/DsSdkApplication.java
@@ -0,0 +1,13 @@
+package com.bluecloud.vnet.sdk.java.dssdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluecloud/vnet/sdk/java/dssdk/DsSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluecloud.vnet.sdk.java.dssdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
